aacbb0471805c5e02c7dc3ac517782921d10ac95
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-06 00:51:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  { key: "value"}
+  {key: "value"}
 end
 
 def pioneer
@@ -16,5 +16,5 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
-  {:id => number}
+  {id: number}
 end
